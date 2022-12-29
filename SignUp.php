c37d0b91c8bba84eb698fa25e31cd6780be9c2df
unknown <salwayehyaabuafash@gmail.com>
2022-12-29 11:49:41 +0200

    register

---
+++ SignUp.php
@@ -0,0 +1,147 @@
+<?php
+require('config.php');
+ 
+  if (isset($_POST['Submit'])) {
+    $Email=$_POST['email'];
+    $Name=$_POST['name'];
+    $Password=$_POST['password'];
+    $Confirm=$_POST['Cpassword'];
+
+
+// to join all if conditions togather  "flags"
+  $one =0;
+  $two =0;
+  $three =0;
+  $four =0;
+
+  $errorName ="";
+  $errorPassword ="";
+  $errorCPassword ="";
+  $errorEmail ="";
+  
+
+//Email 
+if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) && !empty ($_POST['email'])) {
+  $Email=$_POST['email'];
+  $one = 1;
+ }
+
+else {
+ $errorEmail = "Uncorrect ";
+}
+
+//Name
+ if(preg_match("/^[A-Z a-z]+$/" ,$_POST['name']) && !empty($_POST['name'])) {
+  $Name=$_POST['name'];
+  $two = 1;
+ }
+else {
+ $errorName = "not alphabit";
+}
+
+
+
+// password
+//Minimum eight characters, at least one letter, one number and one special character:
+if(preg_match("/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/" ,$_POST['password']) && !empty($_POST['password'])) {
+  $three =1;
+ }
+else {
+ $errorPassword= "Week Password";
+}
+
+
+// Comfirm password
+if( $_POST['password'] == $_POST['Cpassword']) {
+  $four = 1;
+ }
+else {
+ $errorCPassword= "Not Match";
+}
+
+
+// send data after submit
+if ($one ==1 && $two ==1 && $three ==1 && $four ==1  ){
+   $sql="INSERT INTO register (id,name,,email,Password) 
+   VALUES (NULL,:name,:email ,:password)";
+   $statement =  $con->prepare($sql);
+ //binding : bind Varible with query   اربط المتغير مع القيمة
+   $statement->bindValue('name' ,$Name);
+   $statement->bindValue('email' , $Email);
+   $statement->bindValue('password' ,$Password);
+  
+   
+
+   $statement-> execute();     //نفذ
+   header("location:login.php");
+   exit;
+   echo "success";
+  } 
+  else {
+    echo "Not success";
+}
+
+
+};
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="style.css"> -->
+    <title>Sign up</title>
+    <style><?php include('style.css')?></style>
+</head>
+<body>
+
+  <div class="wel">
+    <h1>Sign Up</h1>
+    <p>Create Account , its free</p>
+    </div>
+    <form action="" method="POST">
+<div class="form_lebal">
+    <label  class="form-label ">Name</label>
+    <input type="text" class="form-control" name="name" >
+    <?php if(!empty ($errorName)){
+       echo " <p id='ahmad'>$$errorName</p>" ;
+    }
+    ?>
+  </div>
+  <div class="form_lebal">
+    <label  class="form-label">Email</label>
+    <input type="email" class="form-control" name="email" >
+    <?php if(!empty ($errorEmail)){
+       echo "<p id='ahmad'>$$errorEmail</p>"; 
+    }
+    ?>
+  </div>
+  
+
+  <div class="form_lebal">
+    <label class="form-label">Password</label>
+    <input type="password" class="form-control" name="password">
+    <?php if(!empty ($errorPassword)){
+       echo " <p id='ahmad'>$errorPassword</p>" ;
+    }
+    ?>
+  </div>
+  <div class="form_lebal">
+    <label  class="form-label">Confirm Password</label>
+    <input type="password" class="form-control" name="Cpassword">
+    <?php if(!empty ($errorCPassword)){
+       echo " <p id='ahmad'>$errorCPassword</p>" ;
+    }
+    ?>
+    <br>
+  </div>
+
+  <input class="btn btn-danger col-3 mx-auto icon"  type="Submit" name="Submit" value="SignUp" ><br>
+  <!-- <button type="button" class="btn btn-danger col-3 mx-auto icon" >Sign Up</button><br> -->
+  <p class="wel">Already have an account? <a href="login.php">log in</a></p>
+</form>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,86 @@
+<?php
+session_start();
+require('config.php');
+
+if(isset($_POST['Submit'])){
+  $_SESSION['validate']=false;
+  $Email=$_POST['email'];
+  $Password=$_POST['password'];
+  $error="";
+  $sql="SELECT * FROM user WHERE  Email =:email  and Password =:password ";
+
+   $statement =  $con->prepare($sql); //سرعه بالقراءه و سكيور , بدون تنفيذ
+ //binding : bind Varible with query
+   $statement->bindValue(':email' , $Email);
+   $statement->bindValue(':password' ,$Password);
+   $statement-> execute();    //بتنفذ كود 
+   
+   $S =  $statement->fetch(PDO::FETCH_ASSOC);    //بجيب الداتا  // fetch as array
+  
+  if(!empty($_POST['email']) && !empty($_POST['password']) && ($_SERVER['REQUEST_METHOD'] === 'POST')){
+    
+        if($S){
+        $_SESSION['id']=$S["ID"];
+        $_SESSION['username']=$S["FirstName"];
+        $_SESSION['email']=$S["Email"];
+        $_SESSION['password']=$S["Password"];
+        $_SESSION['role']=$S["Role"];
+        
+        $_SESSION['validate']=true;
+        // echo 'Login Successfully';
+        header('location:user.php');
+
+        //add date last log in use now() function
+        // $statement="UPDATE  users SET  last_login =now() WHERE id=". $_SESSION['ID'];
+        // $statement =  $con->prepare($sql);
+        // $statement->execute();
+          }
+
+        else{
+           $error= "Make sure that you are registere!";
+          }
+      
+        }
+  else{
+    $error ="error";
+  }
+}
+
+ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Log in</title>
+</head>
+<body>
+  <div class="wel">
+    <h1>login</h1>
+    <p>welcome back Login with your credentials</p>
+</div>
+<form action="" method="POST">
+  <div class="mb-3 ">
+    <label class="form-label">Email</label>
+    <input type="text" class="form-control" name="email">
+    <div>abc@gmail.com</div>
+  </div>
+  <div class="mb-3">
+    <label  class="form-label">Password</label>
+    <input type="password" class="form-control" name="password" >
+    </div>
+    <?php 
+    if( !empty ($error) ){
+        echo "<p>$error</p>";
+    }
+    ?>
+  <input class="btn btn-primary col-3 mx-auto icon"  type="Submit" name="Submit" value="Login" >
+
+  <br>
+  <p class="wel">Don't have an account ?<a href="SignUp.php">Sign Up</a> </p>
+</form>
+</body>
+</html>
