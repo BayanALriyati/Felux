4d093b11fab35c2721eddade5119fba5daea5999
unknown <bra2aesam1994@gmail.com>
2022-12-29 16:59:21 +0300

    test

--- config.php
+++ config.php
@@ -3,6 +3,8 @@ $dsn ='mysql:host=localhost;dbname=felux'; //data sourse name
 $user= 'root'; 
 $pass ='';
 
+
+
 try{
     $con = new PDO($dsn ,$user ,$pass);//start new connection 
     $con ->setAttribute(PDO::ATTR_ERRMODE ,PDO::ERRMODE_EXCEPTION);// set the PDO error mode to exception 
