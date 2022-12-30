308e969aa39f4ea9c0d3ca3518f023db7a3e3ff7
unknown <bra2aesam1994@gmail.com>
2022-12-30 10:49:56 +0300

    11

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -8,6 +8,7 @@
   </title>
   <!--     Fonts and icons     -->
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
+
   <!-- Font Awesome Icons -->
   <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
   <!-- Material Icons -->
@@ -20,7 +21,3 @@
   <?php include('sidebar.php')?>
   <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
       <?php include('navbar.php')?>
-
-    
-
-    
